6e45a558a3cf00d1130b5529a8b4285658b4e01e
James Arbogast <jamesarbogast52@gmail.com>
2021-08-10 16:01:00 -0700

    working delete and add

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,11 +25,25 @@ namespace CRUDelicious.Controllers
             ViewBag.AllDishes = dbContext.Dishes.ToList();
             return View();
         }
+
+        [HttpGet("/home")]
+        public IActionResult Home()
+        {
+            //Get all users
+            ViewBag.AllDishes = dbContext.Dishes.ToList();
+            return View("Index");
+        }
         [HttpGet("/new")]
         public IActionResult NewDish(Dish newDish)
         {
             ViewBag.Dish = newDish;
             return View("AddDish");
+        }
+        [HttpGet("/dish/{dishID}")]
+        public IActionResult ViewDish(Dish dish)
+        {
+            ViewBag.Dish = dbContext.Dishes.FirstOrDefault(d => d.DishId == dish.DishId);
+            return View("NewDish", dish);
         }
         [HttpPost("/create")]
         public IActionResult CreateDish(Dish newDish)
@@ -38,6 +52,34 @@ namespace CRUDelicious.Controllers
             ViewBag.Dish = newDish;
             dbContext.SaveChanges();
             return View("NewDish", newDish);
+        }
+        [HttpGet("/delete/{dishID}")]
+        public IActionResult DeleteDish(int dishID)
+        {
+            Dish RetrievedDish = dbContext.Dishes
+                .SingleOrDefault(d => d.DishId == dishID);
+            
+            // Then pass the object we queried for to .Remove() on Users
+            dbContext.Dishes.Remove(RetrievedDish);
+            
+            // Finally, .SaveChanges() will remove the corresponding row representing this User from DB 
+            dbContext.SaveChanges();
+            ViewBag.AllDishes = dbContext.Dishes.ToList();
+            return RedirectToAction("");
+        }
+        [HttpGet("/edit/{dishId}")]
+        public IActionResult EditDish(int dishId)
+        {
+            Dish RetrievedDish = dbContext.Dishes
+            .FirstOrDefault(dish => dish.DishId == dishId);
+            // Then we may modify properties of this tracked model object
+            RetrievedDish.Name = "New name";
+            RetrievedDish.UpdatedAt = DateTime.Now;
+            
+            // Finally, .SaveChanges() will update the DB with these new values
+            dbContext.SaveChanges();
+
+            return View("/dish/{dishID}");
         }
 
         public IActionResult Privacy()
--- Models/MyContext.cs
+++ Models/MyContext.cs
@@ -6,7 +6,6 @@ namespace CRUDelicious.Models
     public class MyContext : DbContext 
     { 
         public MyContext(DbContextOptions options) : base(options) { }
-        // the "Monsters" table name will come from the DbSet variable name
         public DbSet<Dish> Dishes { get; set; }
     }
 }
